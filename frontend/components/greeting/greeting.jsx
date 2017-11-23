dd6b3b50631e675d1dcf2042f26539464ff079fc
Tyler Chi <itychi@gmail.com>
2017-11-22 17:23:55 -0800

    demo

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -4,16 +4,16 @@ import { Link } from 'react-router-dom';
 const sessionLinks = () => (
   <nav className="login-signup">
     <Link to="/login" className = "li-su">Login</Link>
-  
+
     <Link to="/signup" className = "li-su">Sign up!</Link>
   </nav>
 );
 
 const personalGreeting = (currentUser, logout) => (
-	<hgroup className="header-group">
-    <p className="header-name">Hi, {currentUser.username}!</p>
-    <button className="header-button" onClick={logout}>Log Out</button>
-	</hgroup>
+	<div className="logged-in-header-right">
+    <p ></p>Hi, {currentUser.username}</p>
+    <button className="li-su" onClick={logout}>Log Out</button>
+	</div>
 );
 
 const Greeting = ({ currentUser, logout }) => (
