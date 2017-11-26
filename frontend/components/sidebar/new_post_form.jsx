6b72619fcb9e869c2e7b7072dfe387e63955f556
Tyler Chi <itychi@gmail.com>
2017-11-25 17:01:30 -0800

    basic outline of the form lol

--- frontend/components/sidebar/new_post_form.jsx
+++ frontend/components/sidebar/new_post_form.jsx
@@ -4,16 +4,19 @@ class NewPostForm extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      description: "",
+      description: "a",
       img_url: "",
       authorId: currentUser.id
     }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(e) {
     e.preventDefault();
     const newPost = Object.assign({}, this.state);
-    this.props.createPost(post).then(
+
+    this.props.createPost(newPost).then(
       () => this.setState({
         description: "",
         img_url: "",
@@ -22,7 +25,9 @@ class NewPostForm extends React.Component {
     )
   }
 
-
+  update(property) {
+    return e => this.setState({[property]: e.target.value})
+  }
 
 
 
@@ -32,6 +37,38 @@ class NewPostForm extends React.Component {
       <h1>the name's post form. NEW post form.</h1>
       current user: {currentUser.id}
       author id: {this.state.authorId}
+
+      <br/>
+      <br/>
+      <br/>
+
+
+      <form className='new-post-form' onSubmit={this.handleSubmit}>
+
+        <label>Image Url
+          <input
+            ref="img_url"
+            value= {this.state.img_url}
+            placeholder="image url"
+            onChange={ this.update('img_url')}
+          />
+        </label>
+            <br/>
+        <label>Description
+          <input
+            ref="description"
+            value= {this.state.description}
+            placeholder="description"
+            onChange={ this.update('description')}
+          />
+        </label>
+
+
+          <br/>
+
+        <button className = 'new-post-submit-button'>add post</button>
+      </form>
+
     </div>
     )
   }
