ce901cbe686b56f16f4abda0953dc50f5439bc7e
Tyler Chi <itychi@gmail.com>
2017-11-27 16:20:06 -0800

    fixed....stuff...

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,33 @@
+class Api::CommentsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def create
+      @comment = Comment.new
+      @comment.writer_id = current_user.id
+      @comment.post_id = params[:id]
+      @comment.body = params[:body]
+
+      if @comment.save
+        @post = @comment.parent_post
+        render 'api/posts/show'
+      else
+        render json: @comment.errors.full_messages, status: 401
+      end
+  end
+
+  def destroy
+  end
+end
